b31d333edc67ba685dd916bde888074279a98985
Lin Hu <linhu@ca.ibm.com>
2020-06-25 16:45:29 -0400

    Revert "Sampling allocation bytes precisely - step1"
    This reverts commit 7a930ae200c144b206fbf18e7b6c388b13231950.

--- runtime/oti/j9nonbuilder.h
+++ runtime/oti/j9nonbuilder.h
@@ -2780,7 +2780,6 @@ typedef struct J9GCThreadInfo {
 typedef struct J9ModronThreadLocalHeap {
 	U_8* heapBase;
 	U_8* realHeapAlloc;
-	U_8* realHeapTop;
 	UDATA objectFlags;
 	UDATA refreshSize;
 	void* memorySubSpace;
