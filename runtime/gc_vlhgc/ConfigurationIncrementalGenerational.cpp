c6f460ca448694cef9d31ea608014d6f50b8f7db
lhu <linhu@ca.ibm.com>
2024-12-06 08:59:50 -0500

    testing
    extensions->isVirtualLargeObjectHeapRequested = true;
    xint = TRUE;
    
    Signed-off-by: lhu <linhu@ca.ibm.com>

--- runtime/gc_vlhgc/ConfigurationIncrementalGenerational.cpp
+++ runtime/gc_vlhgc/ConfigurationIncrementalGenerational.cpp
@@ -137,6 +137,9 @@ MM_ConfigurationIncrementalGenerational::createHeapWithManager(MM_EnvironmentBas
 #if defined(J9VM_ENV_DATA64)
 	extensions->indexableObjectModel.setIsDataAddressPresent(true);
 #if defined(J9VM_GC_SPARSE_HEAP_ALLOCATION)
+
+	extensions->isVirtualLargeObjectHeapRequested = true;
+
 	if (extensions->isVirtualLargeObjectHeapRequested) {
 		/* Create off-heap */
 		MM_SparseVirtualMemory *largeObjectVirtualMemory = MM_SparseVirtualMemory::newInstance(env, OMRMEM_CATEGORY_MM_RUNTIME_HEAP, heap);
