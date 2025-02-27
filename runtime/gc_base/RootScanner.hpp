056895d4b8cf614bbba337cf26e5a0a6c9f2a1a6
lhu <linhu@ca.ibm.com>
2025-02-27 13:29:25 -0500

    version 4
    Change-Id: Ibc6bdf2e127981d31a18ed9d6f03390fceeb4fe6
    Signed-off-by: lhu <linhu@ca.ibm.com>

--- runtime/gc_base/RootScanner.hpp
+++ runtime/gc_base/RootScanner.hpp
@@ -97,6 +97,9 @@ protected:
 #if defined(J9VM_GC_SPARSE_HEAP_ALLOCATION)
 	bool _includeVirtualLargeObjectHeap; /**< Enables scanning of objects that has been allocated at sparse heap. Default is false */
 #endif /* defined(J9VM_GC_SPARSE_HEAP_ALLOCATION) */
+#if defined(J9VM_GC_ENABLE_DOUBLE_MAP)
+	bool _includeDoubleMap; /**< Enables doublemap should the GC policy be balanced. Default is false. */
+#endif /* J9VM_GC_ENABLE_DOUBLE_MAP */
 	bool _trackVisibleStackFrameDepth; /**< Should the stack walker be told to track the visible frame depth. Default false, should set to true when doing JVMTI walks that report stack slots */
 
 	U_64 _entityStartScanTime; /**< The start time of the scan of the current scanning entity, or 0 if no entity is being scanned.  Defaults to 0. */
@@ -322,6 +325,9 @@ public:
 #if defined(J9VM_GC_SPARSE_HEAP_ALLOCATION)
 		, _includeVirtualLargeObjectHeap(_extensions->indexableObjectModel.isVirtualLargeObjectHeapEnabled())
 #endif /* defined(J9VM_GC_SPARSE_HEAP_ALLOCATION) */
+#if defined(J9VM_GC_ENABLE_DOUBLE_MAP)
+		, _includeDoubleMap(_extensions->indexableObjectModel.isDoubleMappingEnabled())
+#endif /* J9VM_GC_ENABLE_DOUBLE_MAP */
 		, _trackVisibleStackFrameDepth(false)
 		, _entityStartScanTime(0)
 		, _entityIncrementStartTime(0)
@@ -482,6 +488,19 @@ public:
 	 */
 	void scanObjectsInVirtualLargeObjectHeap(MM_EnvironmentBase *env);
 #endif /* defined(J9VM_GC_SPARSE_HEAP_ALLOCATION) */
+
+#if defined(J9VM_GC_ENABLE_DOUBLE_MAP)
+	/**
+	 * Scans each heap region for arraylet leaves that contains a not NULL
+	 * contiguous address. This address points to a contiguous representation
+	 * of the arraylet associated with this leaf. Only arraylets that has been
+	 * double mapped will contain such contiguous address, otherwise the
+	 * address will be NULL
+	 *
+	 * @param env thread GC Environment
+	 */
+	void scanDoubleMappedObjects(MM_EnvironmentBase *env);
+#endif /* J9VM_GC_ENABLE_DOUBLE_MAP */
 
 	virtual void doClassLoader(J9ClassLoader *classLoader);
 
